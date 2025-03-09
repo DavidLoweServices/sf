9f68b676ec2949a9508ced1aecdd637c2237a89f
WannabookAzure <146328286+WannabookAzure@users.noreply.github.com>
2025-03-09 16:38:03 +0000

    test csp

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;",
+          },
+        ],
+      },
+    ];
+  },
   /* config options here */
 };
 
 export default nextConfig;
+
+
+// <meta http-equiv="Content-Security-Policy" content="">
