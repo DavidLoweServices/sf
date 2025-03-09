22068243e6b7f682aea485f588220296755ee5dd
WannabookAzure <146328286+WannabookAzure@users.noreply.github.com>
2025-03-09 16:28:57 +0000

    test es

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,12 @@ export default function Home() {
         {/* Action Button */}
         <div>
             {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-            <Link 
+            <a 
               href="/api/auth/login"
               className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
             >
               Sign In
-            </Link>
+            </a>
         </div>
       </div>
     </div>
